2f00eade6fd584c1da734872c4fa9ffde9035136
Graviton31 <arsenijpiskaev@gmail.com>
2024-11-23 15:31:52 +0300

    changed the _Layout

--- ElectronicJournalApp/Program.cs
+++ ElectronicJournalApp/Program.cs
@@ -27,12 +27,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Auth}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
